fa314b24f11976ad7d20da23d8d68498a049314c
DESKTOP-KM80R2C\kkk <seniorogbonna@gmail.com>
2022-12-24 18:31:01 +0000

    edit and delete communication

--- src/app/pages/customer-service/customer-service/components/followup-history/edit-history/edit-history.component.ts
+++ src/app/pages/customer-service/customer-service/components/followup-history/edit-history/edit-history.component.ts
@@ -19,6 +19,7 @@ export class EditHistoryComponent implements OnInit {
   constructor(public policyCardService: PolicyCardService) {}
 
   @Input() pageControl = '';
+  @Input() actionFlag: number;
   @Input() data = <PolicyCard>{};
   @Output() pageControlChange = new EventEmitter<any>();
 
@@ -31,8 +32,21 @@ export class EditHistoryComponent implements OnInit {
   detailContent: string = '';
   policyPrice: string = '';
   additionalDetailContent: string = '';
+  toEditCommunciation: any;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.actionFlag !== -1) {
+      this.toEditCommunciation = Object.values(this.data.detailsJson)[
+        this.actionFlag
+      ];
+      this.response = this.toEditCommunciation.response;
+      this.detailContent = this.toEditCommunciation.detailContent;
+      this.policyPrice = this.toEditCommunciation.policyPrice;
+      this.additionalDetailContent =
+        this.toEditCommunciation.additionalDetailContent;
+      console.log('resonse', this.response);
+    }
+  }
 
   pageEvent(flag: string) {
     if (flag == 'request') {
@@ -51,8 +65,23 @@ export class EditHistoryComponent implements OnInit {
     this.response = res;
   }
 
+  editCommunciation(currentArray: {}[]) {
+    const detail = {
+      id: this.actionFlag,
+      response: this.response,
+      detailContent: this.detailContent,
+      policyPrice: this.policyPrice,
+      additionalDetailContent: this.additionalDetailContent,
+      date: new Date(),
+    };
+    currentArray.splice(this.actionFlag, 1, detail);
+    this.data.detailsJson = Object.assign({}, currentArray);
+    this.policyCardService.updatePolicyRenewalTickets(this.data);
+  }
+
   createNewCommunications(currentArray: {}[]) {
     const detail = {
+      id: currentArray.length,
       response: this.response,
       detailContent: this.detailContent,
       policyPrice: this.policyPrice,
@@ -60,19 +89,28 @@ export class EditHistoryComponent implements OnInit {
       date: new Date(),
     };
     currentArray.push(detail);
+
     this.data.detailsJson = Object.assign({}, currentArray);
     this.policyCardService.updatePolicyRenewalTickets(this.data);
   }
 
   saveHistory() {
     let currentCommunications: {}[] = [];
-    if (
-      Object.keys(this.data.detailsJson).length === 0 &&
-      this.data.detailsJson.constructor === Object
-    ) {
-      this.createNewCommunications(currentCommunications);
-    } else {
-      this.createNewCommunications(Object.values(this.data.detailsJson));
+
+    switch (this.actionFlag) {
+      case -1:
+        if (
+          Object.keys(this.data.detailsJson).length === 0 &&
+          this.data.detailsJson.constructor === Object
+        ) {
+          this.createNewCommunications(currentCommunications);
+        } else {
+          this.createNewCommunications(Object.values(this.data.detailsJson));
+        }
+        break;
+      default:
+        this.editCommunciation(Object.values(this.data.detailsJson));
+        break;
     }
 
     this.pageControlChange.emit('first');
