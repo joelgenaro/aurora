d45309c0e003e1040b61a65a669e6c928d10827f
regina <seniordeveloper754@gmail.com>
2023-01-17 01:49:34 -0500

    remove alert of https

--- src/app/pages/customer-service/policy-renewals/services/policy-card.service.ts
+++ src/app/pages/customer-service/policy-renewals/services/policy-card.service.ts
@@ -60,7 +60,7 @@ export class PolicyCardService {
       // Get server-side error
       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
-    window.alert(errorMessage);
+    // window.alert(errorMessage);
     return throwError(() => {
       return errorMessage;
     });
